cd77f588118d878922334c68ffc7146630ae3702
Naveen Peruri <58759988+Naveen389@users.noreply.github.com>
2021-10-04 16:30:03 +0530

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Text("Home Page"),
         centerTitle: true,
       ),
       body: Container(),
